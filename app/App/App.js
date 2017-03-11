122a03a8e8d78004e1e52177686a3503834e45d8
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-03-11 15:45:38 +0530

    Added Home And Editor Pages

--- app/App/App.js
+++ app/App/App.js
@@ -6,10 +6,30 @@ import {
 
 
 import Editor from '../components/Editor/Editor';
+import Home from '../components/Home/Home';
 
 const App = () => (
   <Router>
-    <Route path="/editor" component={Editor} />
+    <div id="main">
+      <div id="flex1">
+        <div id="heading">Vim-<span id="heading-bold">me</span></div>
+
+        <div id="terminal-background">
+          <div id="content"></div>
+          <div id="terminal">
+            <Route exact path="/" component={Editor} />
+            <Route path="/quit" component={Home} />
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div>
+          <ul>
+            <li><a href=".default">R</a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>
   </Router>
 );
 
---
+++ app/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <div id="editor">Click to continue</div>
+    );
+  }
+}
+
+export default Home;
