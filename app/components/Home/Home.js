f23e8ba8bab96d35d25b8f8da90fb4250e52d1f4
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-03-11 15:50:15 +0530

    Added Home.js

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React, { PureComponent } from 'react';
 
-class Home extends PureComponent {
-  render() {
-    return (
-      <div id="editor">Click to continue</div>
-    );
-  }
-}
+import { withRouter } from 'react-router-dom';
+
+const Home = withRouter(({ history }) => (
+  <div onClick={() => history.push('/')}>
+    <span id="head-terminal">Vim-me</span>
+    {/* <textarea ref={(c) => { this.textarea = c; }} cols={79} rows={28} id="editor" /> */}
+    <div id="editor">Click To Continue</div>
+    <input disabled={true} id="commands" />
+  </div>
+));
 
 export default Home;
