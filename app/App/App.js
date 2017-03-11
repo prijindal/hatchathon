b903dd047621c8d52186b548a167d958246b4cd5
ssardana08 <sardana.sumit08@gmail.com>
2017-03-11 10:48:48 +0530

    Font size and type set

--- app/App/App.js
+++ app/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => (
   <div id="terminal">
-    <textarea cols={90} rows={30} id="editor"></textarea>
+    <textarea cols={79} rows={28} id="editor"></textarea>
     <br/>
     <input type="text" name="commands" id="commands"></input>
   </div>
