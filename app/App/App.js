09961fe14b12d98742f55847dac3c3724de37a31
ssardana08 <sardana.sumit08@gmail.com>
2017-03-11 12:09:22 +0530

    Changes

--- app/App/App.js
+++ app/App/App.js
@@ -6,8 +6,10 @@ console.log(search('ABCDDABC', 'ABC'));
 
 const App = () => (
 	<div>
-		<h1>Vim-me</h1>
+		<div id="heading">Vim-<span id="heading-bold">me</span></div>
+
 		<div id="terminal">
+			<span id="head-terminal">Vim-me</span>
 			<textarea cols={79} rows={28} id="editor"></textarea>
 			<br/>
 			<input type="text" name="commands" id="commands"></input>
