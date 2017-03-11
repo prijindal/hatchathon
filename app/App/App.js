aa8ee2114f8ad02824327f1283e6618f23a8f864
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-03-11 10:26:49 +0530

    Added App.js

---
+++ app/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="">
+    <div className="">
+      Hello
+    </div>
+  </div>
+);
+
+export default App;
--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App/App';
+
 ReactDOM.render(
-  <div>Hello</div>,
+  <App />,
   document.getElementById('app'),
 );
