5246b87241e5ebdb4688e1f05131c581184eb27d
ssardana08 <sardana.sumit08@gmail.com>
2017-03-11 11:35:55 +0530

    Background added and statusbar added

--- app/App/App.js
+++ app/App/App.js
@@ -5,11 +5,14 @@ import search from '../utils/search';
 console.log(search('ABCDDABC', 'ABC'));
 
 const App = () => (
-  <div id="terminal">
-    <textarea cols={79} rows={28} id="editor"></textarea>
-    <br/>
-    <input type="text" name="commands" id="commands"></input>
-  </div>
+	<div>
+		<h1>Vim-me</h1>
+		<div id="terminal">
+			<textarea cols={79} rows={28} id="editor"></textarea>
+			<br/>
+			<input type="text" name="commands" id="commands"></input>
+		</div>
+	</div>
 );
 
 export default App;
