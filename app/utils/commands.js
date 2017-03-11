d68934d049393df689f09af5fac549719908b74f
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-03-11 14:07:01 +0530

    Fixed Search commands

--- app/utils/commands.js
+++ app/utils/commands.js
@@ -1,12 +1,12 @@
 const searchAction = input => ({
   command: 'search',
-  argument: input.substr(2),
+  argument,
 });
 
 const commands = (input) => {
   if (input[0] === ':') {
     if (input[1] === '?' && input.length > 2) {
-      return searchAction(input);
+      return searchAction(input.substr(2));
     }
     if (input[1] === 'w') {
       return {
@@ -14,8 +14,7 @@ const commands = (input) => {
       };
     }
     if (input[1] === 's' && input.length > 4) {
-      let subCommand = input.substr(5);
-      console.log(subCommand);
+      const subCommand = input.substr(5);
       if (subCommand === 'number') {
         return {
           command: 'setNumber',
@@ -28,7 +27,7 @@ const commands = (input) => {
     }
   }
   if (input[0] === '?' && input.length > 1) {
-    return searchAction(input);
+    return searchAction(input.substr(1));
   }
   return {};
 };
